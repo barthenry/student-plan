e08ff813cda4f45b9b1d2c5899a9ca78a092291f
Mateusz Burzyński <mateuszburzynski@gmail.com>
2016-01-07 00:30:57 +0100

    Small refactor, calendar component extracted

--- client/src/app.js
+++ client/src/app.js
@@ -1,45 +1,46 @@
 import {inject} from 'aurelia-framework';
-import {FetchConfig} from 'aurelia-auth';
-import {AuthorizeStep} from 'aurelia-auth';
+import {FetchConfig, AuthorizeStep} from 'aurelia-auth';
 import {Modal} from './models';
 
 @inject(FetchConfig)
 export class App {
-    loginModal = null;
-    registerModal = null;
+  constructor(fetchConfig) {
+    this.fetchConfig = fetchConfig;
 
-    constructor(fetchConfig){
-        this.fetchConfig = fetchConfig;
-        this.loginModal = new Modal();
-        this.registerModal = new Modal();
+    this.shareModal = new Modal({
+      title: 'Podziel się planem',
+      buttonId: 'shareTriggerButton'
+    });
 
-        this.loginModal
-            .setTitle("Logowanie")
-            .setTriggerButtonId("loginTriggerButton");
+    this.loginModal = new Modal({
+      title: 'Logowanie',
+      buttonId: 'loginTriggerButton'
+    });
 
-        this.registerModal
-            .setTitle("Rejestracja")
-            .setTriggerButtonId("registerTriggerButton");
-    }
+    this.registerModal = new Modal({
+      title: 'Rejestracja',
+      buttonId: 'registerTriggerButton'
+    });
+  }
 
-    activate(){
-        this.fetchConfig.configure();
-    }
+  activate() {
+    this.fetchConfig.configure();
+  }
 
   configureRouter(config, router) {
-      config.title = 'Aurelia';
-      config.addPipelineStep('authorize', AuthorizeStep);
-      
-      config.map([
-          { route: ['', 'creator'], name: 'creator',  moduleId: './plan-creator',  nav: false, title: 'Kreator planu' },
-          { route: 'add-group', name: 'add-group',  moduleId: './add-group',  nav: false, title: 'Dodaj grupę' },
-          { route: 'share',     name: 'share',      moduleId: './share',      nav: false, title: 'Podziel się planem' },
-          { route: 'register',  name: 'register',   moduleId: './register',   nav: false, title: 'Zarejestruj' },
-          { route: 'login',     name: 'login',      moduleId: './login',      nav: false, title: 'Zaloguj' },
-          { route: 'profile',     name: 'profile',      moduleId: './profile',      nav: false, title: 'Profil', auth: true },
-          { route: 'logout',     name: 'logout',      moduleId: './logout',      nav: false, title: 'Logout', auth: true }
-      ]);
+    config.title = 'Student Plan';
+    config.addPipelineStep('authorize', AuthorizeStep);
 
-      this.router = router;
+    config.map([
+      { route: ['', 'creator'], name: 'creator',    moduleId: './plan-creator',   title: 'Kreator planu' },
+      { route: 'add-group',     name: 'add-group',  moduleId: './add-group',      title: 'Dodaj grupę' },
+      { route: 'share',         name: 'share',      moduleId: './share',          title: 'Podziel się planem' },
+      { route: 'register',      name: 'register',   moduleId: './register',       title: 'Zarejestruj' },
+      { route: 'login',         name: 'login',      moduleId: './login',          title: 'Zaloguj' },
+      { route: 'profile',       name: 'profile',    moduleId: './profile',        title: 'Profil', auth: true },
+      { route: 'logout',        name: 'logout',     moduleId: './logout',         title: 'Logout', auth: true }
+    ]);
+
+    this.router = router;
   }
 }
